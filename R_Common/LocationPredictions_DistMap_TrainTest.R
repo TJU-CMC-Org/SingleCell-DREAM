21652b66521a79e98fa2a1bdf25cd751abed3d23
NestorasK <nestor.karathanasis@gmail.com>
2019-09-25 15:24:05 -0400

    update paths, filenames

--- R_Common/LocationPredictions_DistMap_TrainTest.R
+++ R_Common/LocationPredictions_DistMap_TrainTest.R
@@ -29,7 +29,7 @@ source("R_Common/distmap/R/myDistMap.R")
 # Important: quote is set to empty string, this way we can prevent the mis-interpretation of a gene name, that contains an apostrophe
 
 # We convert the data.frame to matrix: each column represents a cell and each row corresponds to a gene.
-raw.data = read.table("Data/dge_raw.txt", sep = "\t", row.names = NULL,
+raw.data = read.table("Data/dge_raw.txt.gz", sep = "\t", row.names = NULL,
                       stringsAsFactors = F, quote = "")
 raw.data.genes = raw.data$V1
 raw.data$V1 = NULL
@@ -44,7 +44,7 @@ raw.data = as.matrix(raw.data)
 rownames(raw.data) = raw.data.genes
 
 # Repeat for the normalised data
-normalized.data = read.table("Data/dge_normalized.txt", sep = "\t",
+normalized.data = read.table("Data/dge_normalized.txt.gz", sep = "\t",
                              row.names = NULL, stringsAsFactors = F, quote = "")
 normalized.data.genes = normalized.data$row.names
 normalized.data$row.names = NULL
@@ -64,7 +64,7 @@ colnames(raw.data) <- colnames(normalized.data)
 stopifnot(all(normalized.data.genes == raw.data.genes))
 
 # Import in situ datasets
-insitu.matrix = read.table("Data/binarized_bdtnp.csv", sep = ",",header = T)
+insitu.matrix = read.table("Data/binarized_bdtnp.csv.gz", sep = ",",header = T)
 
 ## Warning in read.table(gzfile("binarized_bdtnp.csv.gz", "rt"), sep = ",", :
 ## seek on a gzfile connection returned an internal error
@@ -127,14 +127,14 @@ colnames(geometry) = c("x","y","z")
 # Load important features 
 # - Lasso - 
 # 20, 40, 60 genes
-files_cv_results_LASSO <- list.files(path = "SubChallenge_1_3/Results_UniquelyMapped_cells_inSituRNAseq/", 
+files_cv_results_LASSO <- list.files(path = "Modified_LASSO_workflow/Results_UniquelyMapped_cells_inSituRNAseq/", 
                                      pattern = "CV", 
                                      full.names = TRUE)
 
 # - Neural Networks -
 # 20, 40, 60 genes
 files_cv_results_NeuralNets <- 
-    list.files(path = "SubChallenge_2/Results/TopGenes-OnlyInsitus", 
+    list.files(path = "NeuralNetworks/Results_UniquelyMapped_cells_inSituRNAseq", 
                full.names = TRUE, pattern = ".txt", recursive = TRUE)
 
 # - Random - 
