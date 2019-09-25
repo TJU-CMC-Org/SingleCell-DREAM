c62b9a5216f3c3b0f8a9d070609f98a729bb8879
NestorasK <nestor.karathanasis@gmail.com>
2019-09-25 13:56:46 -0400

    update dependencies

--- Modified_LASSO_workflow/R/Dependencies.R
+++ Modified_LASSO_workflow/R/Dependencies.R
@@ -1,5 +1,5 @@
 # Distmap installation
-dependencies <- c("devtools", "DistMap", "data.table", "doMC", "glmnet", "ggplot2", "ggfortify")
+dependencies <- c("devtools", "DistMap", "data.table", "doMC", "glmnet", "ggplot2", "ggfortify", "R.utils")
 new.packages <- dependencies[!(dependencies %in% installed.packages()[,"Package"])]
 if(length(new.packages)) {
     install.packages(new.packages)
